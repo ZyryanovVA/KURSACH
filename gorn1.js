eb01e16692a23b5cf5869aab384ebf7ca790c951
ZyryanovVA <zyryanov.v.2.0.0.0@mail.ru>
2021-12-05 13:23:18 +0300

    'part1'

---
+++ gorn1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="style1.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <meta charset="ANSII">
+    <title>Расписание</title>
+</head>
+<body>
+<img class="CHERTIMAGE" src="Screenshot_1.png" alt="Mоё тестовое изображение">
+</body>
+</html>
